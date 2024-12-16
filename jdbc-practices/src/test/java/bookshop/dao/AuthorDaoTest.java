1e6d57f2aa14c775110bca5dc7a652f3a87a642d
minseokim0629 <han8121449@naver.com>
2024-12-16 20:35:35 +0900

    241216_2 practice bookshop (dao)

---
+++ jdbc-practices/src/test/java/bookshop/dao/AuthorDaoTest.java
@@ -0,0 +1,29 @@
+package bookshop.dao;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import bookshop.vo.AuthorVo;
+
+public class AuthorDaoTest {
+
+	private static AuthorDao dao = new AuthorDao();
+	private static AuthorVo vo = new AuthorVo();
+	
+	@Test
+	public void insertTest() {
+		vo.setName("칼세이건");
+
+		dao.insert(vo);
+
+		assertNotNull(vo.getId());
+	}
+
+	@AfterAll
+	public void cleanup() {
+		dao.deleteById(vo.getId());
+	}
+
+}
